e74e581d829f4bd2ad7dc02e0753fcee42e17f30
mhem-space <mohamed.majjaoui@edu.uah.es>
2022-12-07 14:47:57 +0100

    reset

--- codigo/jm/practica3/arraymeses.js
+++
@@ -1,6 +0,0 @@
-var meses=["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-
-for (i = 0; i < 12; i++) {
-    alert( meses[i]);
-}
-
--- codigo/jm/practica7/ejercicio 7.js
+++
@@ -1,6 +0,0 @@
-var factorial;
-alert (numero=prompt("introduce un numero "));
-for(i=numero-1; i>0; i--){
-factorial= numero*i--;
-}
-alert(factorial);
