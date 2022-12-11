9c2bfb25178e38a009d83d17b288e0d18c2bb4c2
mhem-space <mohamed.majjaoui@edu.uah.es>
2022-12-11 12:31:36 +0100

    eliminada carpeta innecesaria (jm)

--- codigoWeb/jm/practica1/codigo.js
+++
@@ -1,4 +0,0 @@
-alert("HOLA, MUNDO!!!!!");
-alert("Soy el primer script");
-
-//En este ejercicio queremos que la pagina  cree unos cuadros de diálogo emergentes a través de los cuales nos envíe ciertos mensajes. En este caso, nosotros haremos que salude y que diga que es el primer script que hacemos. Para eso, usaremos el comando alert que se encargará de crear los cuadros en los que aparecerá nuestros mensajes
--- codigoWeb/jm/practica12/ejercicio 12.js
+++
@@ -1,6 +0,0 @@
-function muestra () {
-	var elemento = document.getElementById("adicional");
-	elemento.className = "visible";
-	var enlace = document.getElementById("enlace");
-	enlace.className = "oculto";
-}
